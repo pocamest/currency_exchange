bc343a82b6cfa01fd24349b183619db310fa9395
pocamest <pocamest@yandex.ru>
2025-07-08 23:01:15 +0300

    Add mock server

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from currency-exchange!")
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
 
 
-if __name__ == "__main__":
-    main()
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/currencies':
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+            response_data = [
+                {'id': 0, 'name': "United States dollar", 'code': 'USD', "sign": "$"},
+                {"id": 0, "name": "Euro", "code": "EUR", "sign": "€"},
+            ]
+            self.wfile.write(json.dumps(response_data).encode('utf-8'))
+
+
+# Потом попросить пояснить каждую строчку
+def run_server(port=8000):
+    server_address = ('', port)
+    httpd = HTTPServer(server_address, RequestHandler)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.server_close()
+
+
+if __name__ == '__main__':
+    run_server()
