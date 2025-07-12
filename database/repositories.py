cf93473e8c0d8491b1587070b5e73198ce8e11b2
pocamest <pocamest@yandex.ru>
2025-07-12 19:28:32 +0300

    Add find_by_id() and create() in SQLiteCurrenctRepository

--- database/repositories.py
+++ database/repositories.py
@@ -1,3 +1,4 @@
+import sqlite3
 from abc import ABC, abstractmethod
 from typing import Any
 
@@ -10,7 +11,15 @@ class AbstractCurrencyRepository(ABC):
         pass
 
     @abstractmethod
-    def find_by_code(self, code) -> dict[str, Any] | None:
+    def find_by_code(self, code: str) -> dict[str, Any] | None:
+        pass
+
+    @abstractmethod
+    def find_by_id(self, id: int) -> dict[str, Any] | None:
+        pass
+
+    @abstractmethod
+    def create(self, code: str, full_name: str, sign: str) -> dict[str, Any]:
         pass
 
 
@@ -30,10 +39,39 @@ class SQLiteCurrencyRepository(SQLiteConnectionMixin, AbstractCurrencyRepository
             cursor.execute(query)
             return [dict(row) for row in cursor.fetchall()]
 
-    def find_by_code(self, code) -> dict[str, Any] | None:
+    def find_by_code(self, code: str) -> dict[str, Any] | None:
         with self._create_connection() as conn:
             cursor = conn.cursor()
             query = f'SELECT * FROM {self.Table.NAME} WHERE {self.Table.CODE} = ?'
             cursor.execute(query, (code,))
             row = cursor.fetchone()
             return dict(row) if row else None
+
+    def find_by_id(self, id: int) -> dict[str, Any] | None:
+        with self._create_connection() as conn:
+            cursor = conn.cursor()
+            query = f'SELECT * FROM {self.Table.NAME} WHERE {self.Table.ID} = ?'
+            cursor.execute(query, (id,))
+            row = cursor.fetchone()
+            return dict(row) if row else None
+
+    def create(self, code: str, full_name: str, sign: str) -> dict[str, Any]:
+        query = f"""
+                INSERT INTO {self.Table.NAME} (
+                    {self.Table.CODE},
+                    {self.Table.FULL_NAME},
+                    {self.Table.SIGN}
+                )
+                VALUES (?, ?, ?)
+            """
+        with self._create_connection() as conn:
+            cursor = conn.cursor()
+            try:
+                cursor.execute(query, (code, full_name, sign))
+                created_id = cursor.lastrowid
+                created_currency = self.find_by_id(created_id)
+                if created_currency is None:
+                    raise Exception("Не удалось найти только что созданную валюту")
+                return created_currency
+            except sqlite3.IntegrityError:
+                raise
