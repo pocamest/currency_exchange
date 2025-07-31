1a92db742d2616fe1d5c004e1c41bde8bc8e021f
pocamest <pocamest@yandex.ru>
2025-07-31 21:30:43 +0300

    Add processing DECIMAL in sqlite

--- data/connection.py
+++ data/connection.py
@@ -1,14 +1,29 @@
 import sqlite3
+from decimal import Decimal
 
 from domain import AbstractConnectionFactory
 
 
+def adapt_decimal(d: Decimal) -> str:
+    return str(d)
+
+
+def convert_decimal(b: bytes) -> Decimal:
+    return Decimal(b.decode())
+
+
+sqlite3.register_adapter(Decimal, adapt_decimal)
+sqlite3.register_converter('DECIMAL', convert_decimal)
+
+
 class SQLiteConnectionFactory(AbstractConnectionFactory[sqlite3.Connection]):
-    def __init__(self, db_path: str):
+    def __init__(self, db_path: str) -> None:
         self.db_path = db_path
 
     def create_connection(self) -> sqlite3.Connection:
-        conn = sqlite3.connect(self.db_path)
+        conn = sqlite3.connect(
+            self.db_path, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES
+        )
         conn.execute('PRAGMA foreign_keys = ON')
         conn.row_factory = sqlite3.Row
         return conn
